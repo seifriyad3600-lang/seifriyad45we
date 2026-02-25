9f2858e0004b22ede26cc7f6bba7490c26b6d33d
seifriyad3600-lang <seifriyad3600@gmail.com>
2026-02-25 14:58:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
 const pino = require("pino")
+const qrcode = require("qrcode-terminal")
 
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState("session")
@@ -11,27 +12,28 @@ async function startBot() {
 
     sock.ev.on("creds.update", saveCreds)
 
-    sock.ev.on("connection.update", async (update) => {
-        const { connection } = update
+    sock.ev.on("connection.update", (update) => {
+        const { connection, qr, lastDisconnect } = update
+
+        if (qr) {
+            console.log("📲 Scan this QR:\n")
+            qrcode.generate(qr, { small: true })
+        }
 
         if (connection === "connecting") {
-            console.log("🔄 Connecting to WhatsApp...")
+            console.log("🔄 Connecting...")
         }
 
         if (connection === "open") {
             console.log("✅ SeifBot Connected Successfully!")
         }
+
+        if (connection === "close") {
+            const shouldReconnect =
+                lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
+            if (shouldReconnect) startBot()
+        }
     })
-
-    if (!state.creds.registered) {
-        const phoneNumber = "201144534147" // حط رقمك هنا
-
-        setTimeout(async () => {
-            const code = await sock.requestPairingCode(phoneNumber)
-            console.log("\n📲 Pairing Code:\n")
-            console.log(code)
-        }, 4000)
-    }
 }
 
 startBot()
